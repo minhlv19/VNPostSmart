d1d49b6d32789ee8ae813bd9eac1de30c34b1e05
ThanhMinh <minh8631@gmail.com>
2020-02-13 08:33:52 +0700

    add folder

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Main from './src/theme/main/Main';
 
-AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.registerComponent(appName, () => Main);
