7270bc5f9ab0b59aba1f66f98ac28e4fa7fad5fb
admin <trananh14071999@gmail.com>
2020-03-16 22:49:12 +0700

    anh

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import Main from './src/theme/main/Main';
-import Running from "./src/theme/screen/Home/Running";
+import Contact from './src/theme/screen/Home/Contact';
 import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 
 
--- src/theme/screen/Home/Contact.js
+++ src/theme/screen/Home/Contact.js
@@ -1,15 +1,56 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View, ImageBackground, StatusBar} from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Data from './Data';
 
-class Contact extends Component {
+class FlatListItem extends Component {
     render() {
         return (
             <View>
+                <TouchableOpacity>
+                    <View style={{
+                        flex: 1,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        margin: 10,
+                        borderWidth: 1,
+                        borderColor: '#284EAC',
+                        backgroundColor: '#fff',
+                        elevation: 10,
+                        height: 100,
+                        shadowColor: '#fff',
+                        shadowOpacity: 0.2,
+                    }}>
+                        <Text style={styles.flatListItem}>{this.props.item.name}</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
 
-                <Text>Contact</Text>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    flatListItem: {
+        textAlign: 'center',
+        fontSize: 16,
+    }
+});
+
+export default class Contact extends Component {
+    render() {
+        return (
+            <View style={{flex: 1, marginTop: 22}}>
+                <FlatList
+                    data={Data}
+                    renderItem={({item})=>{
+                        //console.log(`Item = ${JSON.stringify(item)}, index = ${index}`);
+                        return (
+                            <FlatListItem item={item}>
+
+                            </FlatListItem>);
+                    }}
+                >
+                </FlatList>
             </View>
         );
     }
 }
-
-export default Contact;
